b18369dcede0b468b4b217469c79e15f05c5240d
Devilgod18 <54558137+Devilgod18@users.noreply.github.com>
2024-07-19 01:23:02 -0500

    update dis

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-﻿const { Client, Intents } = require('discord.js');
+﻿const { Client, GatewayIntentBits } = require('discord.js');
 const { prefix } = require('./config.json');
 const ytdl = require('@distube/ytdl-core');
 const ytpl = require('ytpl');
-const { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus } = require('@discordjs/voice');
-const YouTube = require("discord-youtube-api");
-const DabiImages = require("dabi-images");
+const { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus, VoiceConnectionStatus } = require('@discordjs/voice');
+const YouTube = require('discord-youtube-api');
+const DabiImages = require('dabi-images');
 const DabiClient = new DabiImages.Client();
 const scdl = require('soundcloud-downloader').default;
 const youtube = new YouTube(process.env.YOUTUBE_API_KEY);
 const queue = new Map();
 require('dotenv').config();
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] });
+const client = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds, 
+        GatewayIntentBits.GuildMessages, 
+        GatewayIntentBits.GuildVoiceStates 
+    ]
+});
 
 client.once('ready', () => {
     console.log('Ready!');
@@ -47,7 +53,7 @@ client.on('messageCreate', async message => {
 
 async function execute(message, serverQueue) {
     const args = message.content.split(' ');
-    const search_string = args.slice(1).join(' ');
+    const searchString = args.slice(1).join(' ');
     const voiceChannel = message.member.voice.channel;
     if (!voiceChannel) return message.channel.send('You need to be in a voice channel to play music!');
     const permissions = voiceChannel.permissionsFor(message.client.user);
@@ -76,10 +82,17 @@ async function execute(message, serverQueue) {
             url: track,
             source: 'soundcloud'
         };
-    } else if (ytpl.validateID(search_string)) {
-        const yt_playlist = await youtube.getPlaylist(search_string);
-        for (const item of yt_playlist) {
-            const songInfo = await youtube.getVideo(item.url);
+    } else if (ytpl.validateID(searchString)) {
+        const ytPlaylist = await youtube.getPlaylist(searchString);
+        const songInfo = await youtube.getVideo(ytPlaylist[0].url);
+        song = {
+            title: songInfo.title,
+            url: songInfo.url,
+            source: 'youtube'
+        };
+        queueContruct.songs.push(song);
+        for (let i = 1; i < ytPlaylist.length; i++) {
+            const songInfo = await youtube.getVideo(ytPlaylist[i].url);
             const song = {
                 title: songInfo.title,
                 url: songInfo.url,
@@ -121,7 +134,7 @@ async function execute(message, serverQueue) {
 }
 
 function skip(message, serverQueue) {
-    if (!message.member.voice.channel) return message.channel.send('You have to be in a voice channel to skip the music!');
+    if (!message.member.voice.channel) return message.channel.send('You have to be in a voice channel to stop the music!');
     if (!serverQueue) return message.channel.send('There is no song that I could skip!');
     serverQueue.player.stop();
 }
@@ -143,7 +156,7 @@ function play(guild, song) {
 
     let resource = null;
     if (song.source === 'youtube') {
-        resource = createAudioResource(ytdl(song.url, { filter: 'audioonly', quality: 'highestaudio' }));
+        resource = createAudioResource(ytdl(song.url, { filter: 'audioonly' }));
     } else if (song.source === 'soundcloud') {
         resource = createAudioResource(song.url);
     }
@@ -153,7 +166,7 @@ function play(guild, song) {
         serverQueue.songs.shift();
         play(guild, serverQueue.songs[0]);
     });
-    serverQueue.player.on('error', error => console.error('Player error:', error));
+    serverQueue.player.on('error', error => console.error(error));
 
     serverQueue.textChannel.send(`Now playing: ${song.title}`);
 }
